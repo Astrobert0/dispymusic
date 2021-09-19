e2fa11e1c6373a82c3a074454470ad2d393f13dd
unknown <sebastianajlds@gmail.com>
2021-09-19 02:00:16 +0100

    a

--- discord_bot.py
+++ discord_bot.py
@@ -3,6 +3,9 @@ import discord
 import os
 
 from discord.channel import VoiceChannel
+from discord.ext import commands
+from discord.ext.commands import bot
+
 
 directory = os.path.dirname(__file__)
 rel_path = "discord_token.txt"
@@ -12,6 +15,15 @@ with open(abs_file_path, 'r') as file:
     token = file.read()
 client =discord.Client()
 
+class voice(commands.Cog):
+
+    @commands.command()
+    async def join(ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    @commands.command()
+    async def leave(ctx):
+        await ctx.voice_client.disconnect()
 
 @client.event
 async def on_ready():
@@ -26,8 +38,7 @@ async def on_message(message):
         await message.channel.send('howdy')
 
     elif message.content.startswith('@join'):
-        async def join(ctx):
-            channel = ctx.author.voice
-
-
+        await voice.join(client)
+    elif message.content.startswith('@stop'):
+        await voice.leave(client)
 client.run(token)
